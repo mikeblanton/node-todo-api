56135976b178eaded0b0fd6cd8c9c05879259a0e
Michael Blanton <mblanton@gmail.com>
2017-09-28 16:48:08 -0700

    initial mongoose implementation

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+var newTodo = new Todo({
+  text: 'Cook dinner',
+  completed: true,
+  completedAt: 1234567890
+});
+
+newTodo.save().then((doc) => {
+  console.log('Saved Todo', doc);
+}, (e) => {
+  console.log('Unable to save Todo', e);
+});
